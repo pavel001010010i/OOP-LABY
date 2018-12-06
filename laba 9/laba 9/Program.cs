e95b0365d4cfe03a994642b5c5743a135cd1a84c
pavel001010010i <43006869+pavel001010010i@users.noreply.github.com>
2018-12-06 15:12:19 +0300

    9 laba

---
+++ laba 9/laba 9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_9
+{
+    class Program
+    {
+        delegate void Message();
+
+        static void Main(string[] args)
+        {
+            Message mes1 = Hello;
+            mes1 += HowAreYou;  // теперь mes1 указывает на два метода
+            mes1(); // вызываются оба метода - Hello и HowAreYou
+            Console.Read();
+        }
+        private static void Hello()
+        {
+            Console.WriteLine("Hello");
+        }
+        private static void HowAreYou()
+        {
+            Console.WriteLine("How are you?");
+        }
+    }
+}
