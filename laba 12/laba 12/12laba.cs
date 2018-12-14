f23ecad424095567b5f6991b10ee1bfcf4a9d834
pavel001010010i <43006869+pavel001010010i@users.noreply.github.com>
2018-12-14 18:27:49 +0300

    11-12 labka

---
+++ laba 12/laba 12/12laba.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_12
+{
+    interface IInfoClass
+    {
+        int Max();
+    }
+    public class Lot: IInfoClass
+    {
+        public static int numCreatObjects = 0;
+        public static int chetMin = 0;
+        public static int chetMax = 0;
+        private int Number;
+        private int Coint;
+        public List<int> listInt = new List<int> { };
+        public int[] array = new int[] { };
+        public List<int> listIntTwo = new List<int> { };
+
+        public int lot1 { get; set; }
+        public int lot2 { get; set; }
+        public int lot3 { get; set; }
+        public int lot4 { get; set; }
+        public int lot5 { get; set; }
+
+        public Lot(int a, int b, int c, int d, int e)
+        {
+            this.lot1 = a;
+            this.lot2 = b;
+            this.lot3 = c;
+            this.lot4 = e;
+            this.lot5 = d;
+        }
+        public int number
+        {
+            get
+            {
+                return Number;
+            }
+            set
+            {
+                if (value > 0 && value < 32)
+                {
+                    if (coint != 2)
+                    {
+                        Number = value;
+                    }
+                    else
+                    {
+                        if (value < 29)
+                        {
+                            Number = value;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Некорректные данные");
+                }
+            }
+        }
+        public int coint
+        {
+            get
+            {
+                return Coint;
+            }
+            set
+            {
+                if (value > 0 && value < 13)
+                {
+                    Coint = value;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректные данные");
+                }
+            }
+        }
+        public int year { get; set; }
+        public Lot()
+        {
+            numCreatObjects++;
+        }
+
+        static Lot()
+        {
+            Lot privConstructor = new Lot(1, 1, 1, true);
+            numCreatObjects++;
+            Console.WriteLine("статик");
+        }
+        private Lot(int a, int b, int c, bool isOne)
+        {
+            isOne = true;
+            listIntTwo.Add(a);
+            listIntTwo.Add(b);
+            listIntTwo.Add(c);
+            numCreatObjects++;
+        }
+        //Использование переменной без ref - операции со ЗНАЧЕНИЕМ 
+        //С ref - с самой ПЕРЕМЕННОЙ 
+        //out можно юзать, если не хватает return'а
+        public static void AboutDate()// вывод инфы о классе
+        {
+            Console.WriteLine("Обьъектов создано:");
+            Console.WriteLine($"{numCreatObjects} штук.");
+        }
+
+        public override string ToString()// служит для получения строкового представления данного объекта
+        {
+            return base.ToString() + " " + this.year + " " + this.coint + " " + this.number; // base = вызов конструктора определенного в базавом классе
+
+        }
+
+        public override bool Equals(object obj) //Метод Equals позволяет сравнить два объекта на равенство
+        {
+            if (obj == null) return false;
+
+            if (this.GetType() != obj.GetType()) return false;//Метод GetType позволяет получить тип данного объекта
+
+            Lot a = (Lot)obj;
+
+            if (this.year == a.year)
+                if (this.coint == a.coint)
+                    if (this.number == a.number)
+                        return true;
+            return false;
+        }
+        //позволяет возвратить некоторое числовое значение
+        //которое будет соответствовать данному объекту или его хэш-код
+        public override int GetHashCode() // override - переопределение метода
+        {
+            int hash = 47;
+            hash += this.year + this.coint + this.number;
+            hash %= 3;
+            return hash;
+        }
+        public partial class PartialClass
+        {
+            public int a = 1;
+        }
+        public partial class PartialClass
+        {
+            public int b = 2;
+        }
+        public void Add(int elem)
+        {
+            this.listInt.Add(elem);
+            Console.Write("добавили {0} к множеству -> ", elem);
+            foreach (int ch in this.listInt)
+                Console.Write(ch + " ");
+            Console.WriteLine("\n");
+            numCreatObjects++;
+        }
+        public void Remove(int elem)
+        {
+            this.listInt.Remove(elem);
+            Console.Write("удалили {0} из множества -> ", elem);
+            foreach (int ch in this.listInt)
+                Console.Write(ch + " ");
+            Console.WriteLine("\n");
+            numCreatObjects++;
+        }
+        public int PrintQuantity()
+        {
+            int quantity = 0;
+            foreach (int ch in this.listInt)
+            {
+                quantity++;
+            }
+            Console.WriteLine("\n" + quantity);
+            numCreatObjects++;
+            return quantity;
+        }
+        public int Max()
+        {
+            int count = 0;
+
+            foreach (int ch in this.listInt)
+            {
+                count += ch;
+
+            }
+
+
+            if (chetMax < count)
+            {
+                chetMax = count; //писваиммс переменной максимальное значение
+            }
+            if (chetMin == 0)
+                chetMin = count;
+            else if (chetMin > count)
+            {
+                chetMin = count;
+            }
+            return count;
+        }
+
+        public int maxSum()
+        {
+            Console.WriteLine("\nМожество: ");
+            foreach (int ch in this.listInt)
+            {
+
+                Console.Write(ch + " ");
+            }
+            Console.WriteLine("\nMAX " + chetMax);
+            return chetMax;
+        }
+        public bool NegativeElem()
+        {
+            numCreatObjects++;
+            bool booleanChangeable = false;
+            foreach (int ch in this.listInt)
+                if (ch < 0)
+                {
+                    booleanChangeable = true;
+                }
+
+            return booleanChangeable;
+        }
+        public bool ExistenceElement(int elem)
+        {
+            bool booleanChangeable = false;
+            foreach (int ch in this.listInt)
+                if (ch == elem)
+                {
+                    booleanChangeable = true;
+                }
+
+            return booleanChangeable;
+        }
+
+        public void Priint(string ser)
+        {
+            Console.WriteLine(lot1 + " " + lot2 + " " + lot3 + " " + lot4 + " " + lot5);
+        }
+        public void Input( string str)
+        {
+            Console.WriteLine(" Вывод строки через параметр : "+str);
+        }
+    }
+}
---
+++ laba 12/laba 12/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace laba_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lot set = new Lot(12, 34, 67, 2, 5);
+            
+            Reflector.InfoClass(set);
+            Reflector.Interfece(set);
+            Reflector.Field(set);
+            Reflector.MethodForType(set);
+            Reflector.Voke(set, "Input");
+        }
+    }
+
+    static class Reflector
+    {
+        static public void InfoClass(object obj)
+        {
+            string text = "";
+            Type t = obj.GetType();
+            Console.WriteLine("-------------- Вся информация о классе ------------\n");
+            string path = @"E:\ООП 3 сем\text.txt";
+            //базовый абстрактный класс, определяющий общий функционал
+            foreach (MemberInfo mi in t.GetMembers())//возвращает все члены типа в виде массива объектов MemberInfo
+            {
+                text = text + mi.DeclaringType + " " + mi.MemberType + " " + mi.Name + '\n';
+            }
+            foreach (MemberInfo mi in t.GetMethods())//возвращает все члены типа в виде массива объектов MemberInfo
+            {
+                 Console.WriteLine(mi.DeclaringType + " " + mi.MemberType + " " + mi.Name);
+
+                text = text + mi.DeclaringType + " " + mi.MemberType + " " + mi.Name + '\n';
+            }
+            using (StreamWriter sw = new StreamWriter(path, false, Encoding.Default))
+            {
+                sw.Write(text);
+            }
+            using (StreamReader sr = new StreamReader(path, Encoding.Default))
+            {
+                text = sr.ReadToEnd();
+            }
+            Console.WriteLine("\n");
+        }
+        static public void Interfece(object obj)
+        {
+            Type t = obj.GetType();
+            Console.WriteLine("\n---------- Реализуемые интерфейсы класса ------------ \n");
+            var im = t.GetInterfaces();
+            foreach (Type tp in im)
+                Console.WriteLine(tp.Name);
+            Console.WriteLine();
+        }
+        static public void Field(object obj)
+        {
+            Type t = obj.GetType();
+            Console.WriteLine("------------ Пoля и свойства класса ------------ \n");
+            foreach (MemberInfo mi in t.GetFields()) //возвращает все поля данного типа в виде массива объектов FieldInfo
+            {
+                Console.WriteLine(mi.DeclaringType + " " + mi.MemberType + " " + mi.Name);
+            }
+            foreach (MemberInfo mi in t.GetProperties())//получает все свойства в виде массива объектов PropertyInfo
+            {
+                Console.WriteLine(mi.DeclaringType + " " + mi.MemberType + " " + mi.Name);
+            }
+            Console.WriteLine("\n");
+        }
+        static public void MethodForType(object obj)
+        {
+            Console.WriteLine("\n\nВведите тип параметра запрашиваемого метода: ");
+            string parametr = Console.ReadLine();
+
+            Type t = obj.GetType();
+
+            // Получаем коллекцию методов
+            MethodInfo[] MArr = t.GetMethods();
+            Console.WriteLine("------------- Список методов класса {0} Типа {1} -------------\n", obj.ToString(), parametr);
+
+            // Вывести методы
+            foreach (MethodInfo m in MArr)
+            {
+                // Вывести параметры методов
+                ParameterInfo[] p = m.GetParameters();
+                for (int i = 0; i < p.Length; i++)
+                {
+                    if (p[i].ParameterType.Name == parametr)
+                    {
+                        Console.Write(m.Name + "(" + p[i].ParameterType.Name + " " + p[i].Name + ")");
+                        if (i + 1 < p.Length) Console.Write(", ");
+                    }
+                }
+                Console.Write("\n");
+            }
+        }
+        static public void Voke(object obj, string methode)
+        {
+            Console.WriteLine("\n\nВызов метода через некоторый метод класса:");
+            try
+            {
+                string path = @"E:\ООП 3 сем\parametr.txt";
+                
+                using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+                {
+                    string param;
+                    param = sr.ReadToEnd();
+
+                    Console.WriteLine(param + " ----------------------");
+                    Type t = obj.GetType();
+
+                    // создаем экземпляр класса 
+                    object ekz = Activator.CreateInstance(typeof(Lot));
+
+                    // получаем метод 
+                    MethodInfo method = t.GetMethod(methode);
+
+                    // вызываем метод, передаем ему значения для параметров и получаем результат
+                    method.Invoke(ekz, new object[] { param });
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
